2f195194d42f19a22aca051626f6f406ab5e9656
vincois <vincois@gmail.com>
2014-09-18 21:08:59 -0500

    Update reverseText.py

--- reverseText.py
+++ reverseText.py
@@ -1,1 +1,6 @@
-'hello world' [::-1]
+#This is based upon input recieve from http://stackoverflow.com/questions/931092/reverse-a-string-in-python
+#'hello world' [::-1]
+
+info = raw_input("What do you want reversed?")
+
+print "%r" % (info[::-1])
